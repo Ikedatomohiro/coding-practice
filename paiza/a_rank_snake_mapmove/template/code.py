81513244d07b484c8c7799b367437287d0be56d0
Tomohiro Ikeda <tomo@TomohironoMacBook-Pro-2.local>
2023-04-01 11:10:21 +0900

    templateを作成

---
+++ paiza/a_rank_snake_mapmove/template/code.py
@@ -0,0 +1,12 @@
+# https://paiza.jp/works/mondai/a_rank_level_up_problems/a_rank_snake_mapmove_step3
+from input1 import _INPUT
+
+def main(*, int=int, input=input):
+
+
+if __name__ == "__main__":
+    import io, sys
+
+    sys.stdin = io.StringIO(_INPUT)
+
+    main()
