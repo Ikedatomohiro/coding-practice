a35ed3e23999895b9ca468357517fca564924600
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-04-07 23:47:16 +0900

    add skill check

---
+++ paiza/skill_check/c039/code.py
@@ -0,0 +1,20 @@
+#
+from input1 import _INPUT
+
+def main(*, int=int, input=input):
+    kihou = list(input())
+    result = 0
+    for moji in kihou:
+        if moji == '/':
+            result += 1
+        elif moji == '<':
+            result += 10
+
+    print(result)
+
+if __name__ == "__main__":
+    import io, sys
+
+    sys.stdin = io.StringIO(_INPUT)
+
+    main()
