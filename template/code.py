80b4dee109ddb10bf7eb7b1e1f8e2310664b3e1b
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-04-21 23:34:21 +0900

    edit template

--- template/code.py
+++ template/code.py
@@ -1,12 +1,5 @@
 #
 from input1 import _INPUT
+import io, sys
+sys.stdin = io.StringIO(_INPUT)
 
-def main(*, int=int, input=input):
-
-
-if __name__ == "__main__":
-    import io, sys
-
-    sys.stdin = io.StringIO(_INPUT)
-
-    main()
