d3c7cb405694bde70eed6096bf5a3ee7227e8403
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-04-22 14:47:32 +0900

    add at coder

---
+++ atcoder/tessoku-book/a01/code.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
+from input1 import _INPUT
+import io, sys
+sys.stdin = io.StringIO(_INPUT)
+
+N = int(input())
+ans = N**2
+print(ans)
